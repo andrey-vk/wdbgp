bbaad33f9356df0741ec1b99e7e175ba0b93559e
Andrey V Kravchuk <andrey@whited.ru>
2026-06-29 18:10:48 +0300

    Tighten ESLint rules: enable no-explicit-any, ban-ts-comment, no-template-shadow, require-default-prop

--- webgui/eslint.config.js
+++ webgui/eslint.config.js
@@ -10,11 +10,11 @@ export default [
   {
     rules: {
       'vue/multi-word-component-names': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'vue/require-default-prop': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'vue/require-default-prop': 'error',
       'vue/attribute-hyphenation': 'off',
-      'vue/no-template-shadow': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
+      'vue/no-template-shadow': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
       'vue/max-attributes-per-line': 'off',
       'vue/singleline-html-element-content-newline': 'off',
       'vue/attributes-order': 'off',
